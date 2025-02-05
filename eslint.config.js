62d3855cda847f1989c9eda7d3022d7336221f3f
marcosarias21 <marcosariasolaiz@gmail.com>
2025-02-05 11:05:14 -0300

    Initialize App

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+    },
+    plugins: {
+      react: pluginReact,
+      "@typescript-eslint": tseslint,
+    },
+    settings: {
+      react: {
+        version: "detect", // Detecta automáticamente la versión de React
+      },
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <section className='w-full max-w-md flex justify-center border-1'>
+      <div>
+        <div>
+          <h1 className='text-2xl font-bold'>Nombre Apellido</h1>
+        </div>
+        <div>
+          <p>Descripcion</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default App
