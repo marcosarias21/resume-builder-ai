a0cd46a80a07db51d45143ba5059c82c3dac71fb
marcosarias21 <marcosariasolaiz@gmail.com>
2025-02-26 18:02:18 -0300

    Refactorize code and add ai to better text descriptions

--- src/components/Forms/ProjectsForm/ProjectsForm.tsx
+++ src/components/Forms/ProjectsForm/ProjectsForm.tsx
@@ -4,15 +4,19 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ProjectsSchema } from "@/schemas/formsSchema";
 import { examplesProjects } from "@/helpers/examples";
+import { generateDescription } from "@/serivces/AIGenerativeText";
 
 const ProjectsForm = () => {
-  const { handleSubmit, register, control } = useForm<z.infer<typeof ProjectsSchema>>({
+  const { handleSubmit, register, control, watch } = useForm<z.infer<typeof ProjectsSchema>>({
     resolver: zodResolver(ProjectsSchema),
     defaultValues: { project: [{ name: "", techStack: "", listDescription: [""] }] },
   });
   const { fields, append, remove } = useFieldArray({  control, name: "project" });
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [descriptionArray, setDescriptionArray] = useState(Array(3).fill(null))
+  const [inputValue, setInputValue] = useState<any[] & string[]>([])
+  const prompt = "Mejorame esta breve descripcion como fuera un curriculum, solo dame una descripcion: "
+  const values = watch()
 
   const onSubmit = (values: z.infer<typeof ProjectsSchema>) => {
     console.log(values);
@@ -41,6 +45,11 @@ const ProjectsForm = () => {
     if(descriptionArray.length == 3) alert('Minimun are 3 descriptions points')
   }
 
+  const betterTextWithAI = async (text: string, index: number) => {
+    const data = await generateDescription(text)
+    setInputValue([...inputValue, {index, text: data}])
+  }
+
   return (
     <div className="min-h-[50%] w-full bg-white border border-gray-300 dark:bg-neutral-800 shadow-xl rounded-lg p-6 lg:p-10">
       <form className="gap-6 min-h-[100%]" onSubmit={handleSubmit(onSubmit)}>
@@ -56,7 +65,7 @@ const ProjectsForm = () => {
           </div>
           <div className="mt-1 flex gap-2 w-full">
             <div className="w-full">
-              <label>Name</label>
+              <label className="font-medium">Project Name</label>
               <input
                 className="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-indigo-500 focus:ring-indigo-500"
                 placeholder="Blog App..."
@@ -66,7 +75,7 @@ const ProjectsForm = () => {
               />
             </div>
             <div className="w-full">
-              <label>Tech Stack</label>
+              <label className="font-medium">Tech Stack</label>
               <input
                 className="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-indigo-500 focus:ring-indigo-500"
                 placeholder="React, TypeScript, Tailwind, SQL, etc..."
@@ -78,15 +87,17 @@ const ProjectsForm = () => {
           </div>
           <div className="mt-5">
             <label className="font-medium">Describe your project in points</label>
-            {descriptionArray.map((_, index) => 
+            {descriptionArray?.map((_, index) => 
               <div key={index} className="flex my-4 items-center">
                 <input
                   className="py-3 px-4 mx-2 block w-full border border-gray-200 rounded-lg text-sm focus:border-indigo-500 focus:ring-indigo-500"
                   type="text"
-                 placeholder={examplesProjects[index] ? examplesProjects[index] : 'Put a litle description...'}
-                   {...register(`project.${currentIndex}.listDescription.${index}`)}
+                  placeholder={examplesProjects[index] ? examplesProjects[index] : 'Put a litle description...'}
+                  {...register(`project.${currentIndex}.listDescription.${index}`)}
+                  value={index === inputValue[index]?.index ? inputValue[index]?.text : null}
+
                 />
-                <button className="bg-purple-500 py-2 px-2 rounded text-white font-bold hover:bg-purple-400">AI</button>
+                <button onClick={() => betterTextWithAI(prompt+values?.project[currentIndex]?.listDescription[index], index)} className="bg-purple-500 py-2 px-2 rounded text-white font-bold hover:bg-purple-400">AI</button>
               </div>
             )}
             </div>
