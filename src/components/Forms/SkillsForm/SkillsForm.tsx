cb7312b1c04d9e2064b52459d49eff767a63ab67
marcosarias21 <marcosariasolaiz@gmail.com>
2025-03-05 10:47:19 -0300

    add scrollbar to mantain same height

--- src/components/Forms/SkillsForm/SkillsForm.tsx
+++ src/components/Forms/SkillsForm/SkillsForm.tsx
@@ -23,23 +23,23 @@ const SkillsForm = () => {
   }
 
   return (
-    <div className='min-h-[50%] w-full bg-white border-1 border-gray-300 border-t-blue-400 border-t-4 dark:bg-neutral-800 shadow-xl rounded-lg p-6 lg:p-10'>
-        <form onSubmit={handleSubmit(onSubmit)} className='gap-6 min-h-[100%]'>             
+    <div className='min-h-[60%] w-full bg-white border-1 border-gray-300 border-t-blue-400 border-t-4 dark:bg-neutral-800 shadow-xl rounded-lg p-6 lg:p-10'>
+        <form onSubmit={handleSubmit(onSubmit)} className='gap-6 h-full'>             
           <div>
             <div className="flex justify-between items-center my-4">
               <label htmlFor="message" className="block mb-2 text-md font-bold text-gray-700 dark:text-gray-300">Add your professional skills</label>
             </div>
+            <div className="grid grid-cols-3 p-5">
              {skillsArray?.map((_, index) => (
               <div key={index} className="mb-1">
-                <div className="flex justify-between border-1 rounded-2xl p-5">
-                  <div className="flex flex-col p-1 w-full">
-                    <label htmlFor="skills" className="font-bold text-gray-700 flex items-center">Skill:<span><Asterisk className="text-red-500" size={14} /></span></label>
-                    <input className='font-medium py-3 px-4 block w-full border-1 border-gray-200 rounded-lg text-sm ffocus:border-green-400 focus:ring-1 focus:ring-green-400 focus:outline-none dark:text-white dark:placeholder-gray-400'
-                    placeholder={examplesSkills[index] ? examplesSkills[index] : "Eg: ..."} type='text' {...register(`skills.${index}`, { required: true })} />
-                  </div>   
-                </div>
+                <div className="flex flex-col p-1 w-full">
+                  <label htmlFor="skills" className="font-bold text-gray-700 flex items-center">Skill:<span><Asterisk className="text-red-500" size={14} /></span></label>
+                  <input className="font-medium py-3 px-4 block w-full border rounded-sm text-sm focus:border-green-400 focus:ring-1 focus:ring-green-400 focus:outline-none dark:text-white dark:placeholder-gray-400"
+                  placeholder={examplesSkills[index] ? examplesSkills[index] : "Add a key skill that you used"} type='text' {...register(`skills.${index}`, { required: true })} />
+                </div>   
               </div>
              ))}
+            </div>
           </div>              
           {errors.skills && (<p className="text-red-500 text-sm mt-2">Fill in all fields to save!</p>)}
           <div className="flex justify-between mt-2">
