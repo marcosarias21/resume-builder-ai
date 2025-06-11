820577c0c42eef65228be736e237b4ea6bd9b9c8
marcosarias21 <marcosariasolaiz@gmail.com>
2025-06-11 14:01:28 -0300

    Se eliminaron console.logs, se agrego carpeta constants para prompts y alerts

---
+++ src/constants/prompts/prompts-to-ai.ts
@@ -0,0 +1,6 @@
+export const PROJECT_PROMPT =
+  "Generate a concise, professional project description for a resume, improving the following text:  Context: ";
+export const SUMMERY_PROMPT =
+  "Generate just an about me as if it were a resume based only on this data:";
+export const WORK_PROMPT =
+  "Generate a summery from work experiences, for a cv with this datas, give me only a description medium-large:";
